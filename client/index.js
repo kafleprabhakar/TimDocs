92a42142dfd23ab6547bad88ee4d051bf36a67b8
natashamaniarnew <114311370+natashamaniarnew@users.noreply.github.com>
2023-04-25 00:26:09 -0400

    very basic text editor for client

--- client/index.js
+++ client/index.js
@@ -2,5 +2,7 @@ window.onload = async function() {
     const response = await fetch("http://localhost:3000");
     const jsonData = await response.json();
     console.log(jsonData);
-    document.getElementById('dummy-p').innerHTML = JSON.stringify(jsonData);
-};
+    var editor = CodeMirror.fromTextArea
+    document.getElementById('editor').mode = "xml"
+    document.getElementById('editor').theme = "dracula"
+}; 
